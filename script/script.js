d68b2a236761ebd6f4fb8117d29ece8febc4125b
Leonardo Silva <leo_ipaiva@outlook.com>
2023-10-23 13:30:49 -0300

    trabalhosite1

---
+++ script/script.js
@@ -0,0 +1,17 @@
+let select = (e) => document.querySelector(e)
+
+select('.link1').addEventListener('click', () =>
+select('.carousel').style.marginLeft = '0vw'
+)
+
+select('.link2').addEventListener('click', () =>
+select('.carousel').style.marginLeft = '-100vw'
+)
+
+select('.link3').addEventListener('click', () =>
+select('.carousel').style.marginLeft = '-200vw'
+)
+
+select('.link4').addEventListener('click', () =>
+select('.carousel').style.marginLeft = '-300vw'
+)
